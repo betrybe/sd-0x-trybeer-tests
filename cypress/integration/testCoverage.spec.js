ecb3cbabba54fcaf5c83aea17a4a784ed27e8d46
bruno batista <brunobatista101@gmail.com>
2020-10-12 17:28:57 -0300

    Update testCoverage.spec.js

--- cypress/integration/testCoverage.spec.js
+++ cypress/integration/testCoverage.spec.js
@@ -5,7 +5,7 @@ describe('A cobertura de testes unitários deve ser de no mínimo 90%', () => {
     });
 
     it('Será validado a cobertura de testes unitários do back-end', () => {
-      cy.exec('npm --prefix back-end/ run test-coverage -- --coverageReporters="json-summary" --testFailureExitCode=0 ');
+      cy.exec('npm --prefix back-end/ run test-coverage -- --coverageReporters="json-summary" --testFailureExitCode=0 --forceExit ');
       cy.readFile('back-end/coverage/coverage-summary.json').its('total.lines.pct').should('be.gte', 90.00);
     });
   });
